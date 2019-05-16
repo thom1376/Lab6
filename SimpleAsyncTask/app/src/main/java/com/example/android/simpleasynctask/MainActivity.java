584ebe4879bcc3cc73f2572620ac0cabd2beba7f
Logan Thomas <ltogan.2014@gmail.com>
2019-05-16 11:31:27 -0400

    Start async task on button press in SimpleAsyncTask project

--- SimpleAsyncTask/app/src/main/java/com/example/android/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/example/android/simpleasynctask/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.android.simpleasynctask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView1);
     }
 
     public void startTask(View view) {
+        mTextView.setText(R.string.napping);
+        new SimpleAsyncTask(mTextView).execute();
     }
 }
