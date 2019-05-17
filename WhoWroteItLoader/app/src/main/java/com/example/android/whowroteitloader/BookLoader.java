7de9d83e6cee269fafdb065bbeef1c6d9189c2eb
Logan Thomas <ltogan.2014@gmail.com>
2019-05-17 01:27:09 -0400

    Add BookLoader class to WhoWroteItLoader project

---
+++ WhoWroteItLoader/app/src/main/java/com/example/android/whowroteitloader/BookLoader.java
@@ -0,0 +1,26 @@
+package com.example.android.whowroteitloader;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+    private String mQueryString;
+
+    public BookLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+}
