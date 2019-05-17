b8a3fb900173007253b9c1ee063f2f155ab03ae1
Logan Thomas <ltogan.2014@gmail.com>
2019-05-16 23:42:34 -0400

    Add FetchBook class WhoWroteIt project

---
+++ WhoWroteIt/app/src/main/java/com/example/android/whowroteit/FetchBook.java
@@ -0,0 +1,26 @@
+package com.example.android.whowroteit;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    FetchBook(TextView titleText, TextView authorText) {
+        this.mTitleText = new WeakReference<>(titleText);
+        this.mAuthorText = new WeakReference<>(authorText);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
