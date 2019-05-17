cb903997eec4c486a71abf8870acb6eae12a154c
Logan Thomas <ltogan.2014@gmail.com>
2019-05-17 04:24:32 -0400

    Receive power connected/disconnected broadcasts in PowerReceiver project

--- PowerReceiver/app/src/main/java/com/example/android/powerreceiver/MainActivity.java
+++ PowerReceiver/app/src/main/java/com/example/android/powerreceiver/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.powerreceiver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private CustomReceiver mReceiver = new CustomReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+
+        // Register the receiver using the activity context.
+        this.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        //Unregister the receiver
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
