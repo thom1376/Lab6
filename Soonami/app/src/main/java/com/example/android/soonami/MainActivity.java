a1b8be6e1d05b7abc25ea2ea008470444bf4fe8a
Logan Thomas <ltogan.2014@gmail.com>
2019-05-22 14:42:17 -0400

    Handle HTTP response codes in Soonami project

--- Soonami/app/src/main/java/com/example/android/soonami/MainActivity.java
+++ Soonami/app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.soonami;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** URL to query the USGS dataset for earthquake information */
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2012-01-01&endtime=2012-12-01&minmagnitude=6";
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-12-01&minmagnitude=7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -154,6 +155,10 @@ public class MainActivity extends AppCompatActivity {
          */
         private String makeHttpRequest(URL url) throws IOException {
             String jsonResponse = "";
+
+            if (url == null)
+                return jsonResponse;
+
             HttpURLConnection urlConnection = null;
             InputStream inputStream = null;
             try {
@@ -162,8 +167,16 @@ public class MainActivity extends AppCompatActivity {
                 urlConnection.setReadTimeout(10000 /* milliseconds */);
                 urlConnection.setConnectTimeout(15000 /* milliseconds */);
                 urlConnection.connect();
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
+
+                // Use switch for the ability to easily handle other response codes in the future
+                switch (urlConnection.getResponseCode()){
+                    case 200:
+                        inputStream = urlConnection.getInputStream();
+                        jsonResponse = readFromStream(inputStream);
+                        break;
+                    default:
+                        break;
+                }
             } catch (IOException e) {
                 // TODO: Handle the exception
             } finally {
@@ -201,6 +214,9 @@ public class MainActivity extends AppCompatActivity {
          * about the first earthquake from the input earthquakeJSON string.
          */
         private Event extractFeatureFromJson(String earthquakeJSON) {
+            if (TextUtils.isEmpty(earthquakeJSON))
+                return null;
+
             try {
                 JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
                 JSONArray featureArray = baseJsonResponse.getJSONArray("features");
